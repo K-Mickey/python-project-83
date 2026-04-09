9c69ba4ef6bc44854791d2ce31bc300a3083f76b
Mikhail Kokuev <kokmickey@yandex.ru>
2026-04-09 18:10:25 +0500

    add link to db

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from contextlib import contextmanager
 
 import psycopg2
 import validators
@@ -14,6 +15,8 @@ from flask import (
     url_for,
 )
 from flask_wtf import CSRFProtect
+from psycopg2.errors import UniqueViolation
+from psycopg2.pool import SimpleConnectionPool
 
 load_dotenv()
 
@@ -27,7 +30,21 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 csrf = CSRFProtect(app)
 
-conn = psycopg2.connect(DATABASE_URL)
+
+pool = SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dsn=DATABASE_URL,
+)
+
+
+@contextmanager
+def get_connection():
+    conn = pool.getconn()
+    try:
+        yield conn
+    finally:
+        pool.putconn(conn)
 
 
 @app.get("/")
@@ -49,11 +66,30 @@ def create_url():
             "index.html",
             url=url,
             errors=errors,
-        )
+        ), 422
+
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO urls (url) VALUES (%s)",
+                    (url,),
+                )
+            conn.commit()
+
+    except UniqueViolation:
+        logger.debug("URL already exists")
+        flash("Такая ссылка уже существует", "danger")
+        return redirect(url_for("index"))
+
+    except Exception as e:
+        logger.error("Error during URL insertion: %s", e)
+        flash("Произошла ошибка при добавлении ссылки", "danger")
+        return redirect(url_for("index"))
 
     flash("Ссылка успешно добавлена", "success")
 
-    return redirect(url_for("index"), code=302)
+    return redirect(url_for("index"), code=303)
 
 
 def validate(url: str) -> dict[str, list[str]]:
