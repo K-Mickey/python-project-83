dd759bc4c3d1a27581c8a74bca4342265331309c
Mikhail Kokuev <kokmickey@yandex.ru>
2026-04-09 17:02:27 +0500

    add csrf token and show errors for urls

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import psycopg2
@@ -12,14 +13,20 @@ from flask import (
     request,
     url_for,
 )
+from flask_wtf import CSRFProtect
 
 load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+MAX_URL_LENGTH = 255
+
+logging.basicConfig(level="DEBUG")
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+csrf = CSRFProtect(app)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 conn = psycopg2.connect(DATABASE_URL)
 
 
@@ -36,9 +43,25 @@ def index():
 def create_url():
     url = request.form.get("url", "")
 
-    if not validators.url(url):
-        flash("Invalid URL", "danger")
-    else:
-        flash("URL created", "success")
+    if errors := validate(url):
+        logger.debug("Validation errors: %s", errors)
+        return render_template(
+            "index.html",
+            url=url,
+            errors=errors,
+        )
+
+    flash("Ссылка успешно добавлена", "success")
 
     return redirect(url_for("index"), code=302)
+
+
+def validate(url: str) -> dict[str, list[str]]:
+    errors = {}
+    if not validators.url(url):
+        errors.setdefault("url", []).append("Некорректная ссылка")
+    if len(url) > MAX_URL_LENGTH:
+        errors.setdefault("url", []).append(
+            f"Ссылка не должна превышать {MAX_URL_LENGTH} символов"
+        )
+    return errors
